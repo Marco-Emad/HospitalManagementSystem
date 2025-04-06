78a08ca0827eecbb37e335e63ea2423bba2043f7
Marco-Emad <marcoemad42@yahoo.com>
2025-04-06 17:31:21 +0200

    Rigestered Repositories in Progam file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using HospitalManagementSystem.Data;
+using HospitalManagementSystem.Interfaces;
+using HospitalManagementSystem.Models;
+using HospitalManagementSystem.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -10,6 +13,9 @@ builder.Services.AddControllersWithViews();
 //Register DbContext
 builder.Services.AddDbContext<HospitalDdContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
 
 
 var app = builder.Build();
